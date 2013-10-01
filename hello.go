58cc675525a7374bbebc73076862cf964b0aabe1
Ingo Jaeckel <ingo.jaeckel@googlemail.com>
2013-10-01 09:22:01 -0400

    a lot of testing..

--- hello.go
+++ hello.go
@@ -2,18 +2,31 @@ package hello
 
 import (
 	"appengine"
+	"encoding/json"
 	"appengine/memcache"
 	"fmt"
+	"strings"
 	"net/http"
+	"strconv"
+	"code.google.com/p/go-uuid/uuid"
+	"github.com/gorilla/mux"
 )
 
 func init() {
-	http.HandleFunc("/hello", hello)
-	http.HandleFunc("/no/content", noContent)
-	http.HandleFunc("/no/content/cache", noContentCache)
-	http.HandleFunc("/hits", showHits)
-	http.HandleFunc("/state/init", initState)
-	http.HandleFunc("/state/get", getState)
+	r := mux.NewRouter()
+
+	r.HandleFunc("/rest/hello", hello)
+	r.HandleFunc("/rest/join", join)
+	r.HandleFunc("/rest/poll", poll)
+	r.HandleFunc("/rest/move/{uuid}/{x:[0-9]+}/{y:[0-9]+}", move)
+	r.HandleFunc("/rest/no/content", noContent)
+	r.HandleFunc("/rest/no/content/cache", noContentCache)
+	r.HandleFunc("/rest/hits", showHits)
+	r.HandleFunc("/rest/state/init", initState)
+	r.HandleFunc("/rest/state/get", getState)
+	r.HandleFunc("/rest/json", getJson)
+
+	http.Handle("/", r)
 }
 
 type LeaderboardEntry struct {
@@ -23,11 +36,6 @@ type LeaderboardEntry struct {
 
 type Leaderboard struct {
 	entries []LeaderboardEntry
-}
-
-type State struct {
-	a,b,c,d int
-	state string
 }
 
 func initState(w http.ResponseWriter, r *http.Request) {
@@ -65,6 +73,116 @@ func noContentCache(w http.ResponseWriter, r *http.Request) {
 func hello(w http.ResponseWriter, r *http.Request) {
 	name := r.Header.Get("name")
 	fmt.Fprintf(w, "Hello %s!", name)
+}
+
+func join(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	playerCount, _ := memcache.Increment(c, "playerCount", 1, 0)
+	uuid := uuid.New()
+
+	memcache.JSON.Set(c, &memcache.Item {
+		Key: fmt.Sprintf("player%d", playerCount),
+		Object: uuid,
+	})
+	memcache.JSON.Set(c, &memcache.Item {
+		Key: fmt.Sprintf("%v.x", uuid),
+		Object: 100,
+	})
+	memcache.JSON.Set(c, &memcache.Item {
+		Key: fmt.Sprintf("%v.y", uuid),
+		Object: 100,
+	})
+
+
+	c.Infof("playerCount %d", playerCount)
+	fmt.Fprintf(w, uuid)
+}
+
+func move(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	uuid := vars["uuid"]
+	x, _ := strconv.Atoi(vars["x"])
+	y, _ := strconv.Atoi(vars["y"])
+
+	c := appengine.NewContext(r)
+	
+	memcache.JSON.Set(c, &memcache.Item {
+		Key: fmt.Sprintf("%v.x", uuid),
+		Object: x,
+	})
+	memcache.JSON.Set(c, &memcache.Item {
+		Key: fmt.Sprintf("%v.y", uuid),
+		Object: y,
+	})
+
+	w.WriteHeader(204)
+}
+
+type Position struct {
+	X,Y int
+}
+
+type State struct {
+	Count int
+	Player1, Player2 Position
+}
+
+func getJson(w http.ResponseWriter, r *http.Request) {
+	s := State{23, Position{1,2}, Position{3,4}}
+
+	bytes, _ := json.Marshal(s)
+	fmt.Fprint(w, string(bytes))
+}
+
+func getCoordinates(r * http.Request, key string) (int, int) {
+	c := appengine.NewContext(r)
+
+	var uuid string
+	memcache.JSON.Get(c, key, &uuid)
+
+	keyX1 := fmt.Sprintf("%v.x", uuid)
+	keyY1 := fmt.Sprintf("%v.y", uuid)
+
+	var x,y int
+
+	memcache.JSON.Get(c, keyX1, &x)
+	memcache.JSON.Get(c, keyY1, &y)
+
+	return x,y
+}
+
+func poll(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	
+	var playerCount int
+	
+	memcache.JSON.Get(c, "playerCount", &playerCount)
+
+	if 1 == playerCount {
+		x, y := getCoordinates(r, "player1")
+		s := State{1, Position{x,y}, Position{}}
+
+		// fmt.Printf(s)
+		fmt.Fprintf(w, "%q", s)
+	} else {
+		x1, y1 := getCoordinates(r, "player1")
+		x2, y2 := getCoordinates(r, "player1")
+
+		s := State{1, Position{x1, y1}, Position{x2, y2}}
+
+		// fmt.Printf(s)
+		fmt.Fprintf(w, "%+v", s)
+	}
+}
+
+func getPosition(value string) Position {
+	fmt.Printf("value = %s", value)
+	parts := strings.Split(value, " ")
+	fmt.Printf("len %d", len(parts))
+	x, _ := strconv.Atoi(parts[0])
+	y, _ := strconv.Atoi(parts[1])
+
+	return Position{x, y}
 }
 
 func showHits(w http.ResponseWriter, r *http.Request) {
