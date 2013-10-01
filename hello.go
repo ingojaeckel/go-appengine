43878e38846f54ea4d80a237055a543337bbc3d6
Ingo Jaeckel <ingo.jaeckel@googlemail.com>
2013-10-01 18:06:49 -0400

    first version of poll that works :)

--- hello.go
+++ hello.go
@@ -15,15 +15,9 @@ import (
 func init() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/rest/hello", hello)
 	r.HandleFunc("/rest/join", join)
 	r.HandleFunc("/rest/poll", poll)
 	r.HandleFunc("/rest/move/{uuid}/{x:[0-9]+}/{y:[0-9]+}", move)
-	r.HandleFunc("/rest/no/content", noContent)
-	r.HandleFunc("/rest/no/content/cache", noContentCache)
-	r.HandleFunc("/rest/hits", showHits)
-	r.HandleFunc("/rest/state/init", initState)
-	r.HandleFunc("/rest/state/get", getState)
 	r.HandleFunc("/rest/json", getJson)
 	r.HandleFunc("/rest/put", memcachePut)
 	r.HandleFunc("/rest/read", memcacheRead)
@@ -31,72 +25,26 @@ func init() {
 	http.Handle("/", r)
 }
 
-type LeaderboardEntry struct {
-	player string
-	value int
-}
-
-type Leaderboard struct {
-	entries []LeaderboardEntry
-}
-
-func initState(w http.ResponseWriter, r *http.Request) {
-        c := appengine.NewContext(r)
-
-	var in struct {I int;}
-	in.I = 23
-	item := &memcache.Item {
-		Key: "s",
-		Object: in,
-	}
-	memcache.JSON.Set(c, item)
-	
-	w.WriteHeader(204)
-}
-
-func getState(w http.ResponseWriter, r *http.Request) {
-        c := appengine.NewContext(r)
-
-	var out struct {I int;}
-	memcache.JSON.Get(c, "s", &out)
-	fmt.Fprint(w, out)
-}
-
-func noContent(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(204)
-}
-
-func noContentCache(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Cache-Control", "public, max-age=10")
-	w.Header().Set("Pragma", "Public")
-	w.WriteHeader(204)
-}
-
-func hello(w http.ResponseWriter, r *http.Request) {
-	name := r.Header.Get("name")
-	fmt.Fprintf(w, "Hello %s!", name)
-}
-
 func join(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
 	playerCount, _ := memcache.Increment(c, "playerCount", 1, 0)
 	uuid := uuid.New()
 
+	uuidKey := fmt.Sprintf("player%d", playerCount)
+	posKey := fmt.Sprintf("%v.pos", uuid)
+	c.Warningf("playerCount %d", playerCount)
+	c.Warningf("UUID Key %s", uuidKey)
+	c.Warningf("Pos key %s", posKey)
+
 	memcache.JSON.Set(c, &memcache.Item {
-		Key: fmt.Sprintf("player%d", playerCount),
+		Key: uuidKey,
 		Object: uuid,
 	})
 	memcache.JSON.Set(c, &memcache.Item {
-		Key: fmt.Sprintf("%v.x", uuid),
-		Object: 100,
-	})
-	memcache.JSON.Set(c, &memcache.Item {
-		Key: fmt.Sprintf("%v.y", uuid),
-		Object: 100,
+		Key: posKey,
+		Object: Position{100, 100},
 	})
 
-
-	c.Infof("playerCount %d", playerCount)
 	fmt.Fprintf(w, uuid)
 }
 
@@ -108,13 +56,12 @@ func move(w http.ResponseWriter, r *http.Request) {
 
 	c := appengine.NewContext(r)
 	
+	posKey := fmt.Sprintf("%v.pos", uuid)
+	c.Warningf("Pos key %s", posKey)
+
 	memcache.JSON.Set(c, &memcache.Item {
-		Key: fmt.Sprintf("%v.x", uuid),
-		Object: x,
-	})
-	memcache.JSON.Set(c, &memcache.Item {
-		Key: fmt.Sprintf("%v.y", uuid),
-		Object: y,
+		Key: posKey,
+		Object: Position{x,y},
 	})
 
 	w.WriteHeader(204)
@@ -159,21 +106,22 @@ func memcacheRead(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, string(b2));
 }
 
-func getCoordinates(r * http.Request, key string) (int, int) {
+func getCoordinates(r * http.Request, key string) Position {
 	c := appengine.NewContext(r)
 
 	var uuid string
 	memcache.JSON.Get(c, key, &uuid)
 
-	keyX1 := fmt.Sprintf("%v.x", uuid)
-	keyY1 := fmt.Sprintf("%v.y", uuid)
+	posKey := fmt.Sprintf("%v.pos", uuid)
 
-	var x,y int
+	c.Warningf("UUID %s", uuid)
+	c.Warningf("pos key %s", posKey)
 
-	memcache.JSON.Get(c, keyX1, &x)
-	memcache.JSON.Get(c, keyY1, &y)
+	var pos Position
 
-	return x,y
+	memcache.JSON.Get(c, posKey, &pos)
+
+	return pos
 }
 
 func poll(w http.ResponseWriter, r *http.Request) {
@@ -184,19 +132,18 @@ func poll(w http.ResponseWriter, r *http.Request) {
 	memcache.JSON.Get(c, "playerCount", &playerCount)
 
 	if 1 == playerCount {
-		x, y := getCoordinates(r, "player1")
-		s := State{1, Position{x,y}, Position{}}
+		pos := getCoordinates(r, "player1")
+		s := State{1, pos, Position{0,0}}
 
-		// fmt.Printf(s)
-		fmt.Fprintf(w, "%q", s)
+		bytes, _ := json.Marshal(s)
+		fmt.Fprintf(w, string(bytes))
 	} else {
-		x1, y1 := getCoordinates(r, "player1")
-		x2, y2 := getCoordinates(r, "player1")
+		pos1 := getCoordinates(r, "player1")
+		pos2 := getCoordinates(r, "player1")
 
-		s := State{1, Position{x1, y1}, Position{x2, y2}}
-
-		// fmt.Printf(s)
-		fmt.Fprintf(w, "%+v", s)
+		s := State{2, pos1, pos2}
+		bytes, _ := json.Marshal(s)
+		fmt.Fprintf(w, string(bytes))
 	}
 }
 
@@ -210,12 +157,3 @@ func getPosition(value string) Position {
 	return Position{x, y}
 }
 
-func showHits(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%d", hits(r))
-}
-
-func hits(r *http.Request) uint64 {
-        c := appengine.NewContext(r)
-	newValue, _ := memcache.Increment(c, "hits", 1, 0)
-	return newValue
-}
